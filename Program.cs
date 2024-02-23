a4c748694225095f40242bbe9cf75e520d1429a4
eliudefrancisco14 <eliudepauloquintasf@gmail.com>
2024-02-23 13:21:02 +0100

    Gerenciamento de Tipos de Dados de Data e Hora em C#

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Program
+{
+
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            // pr.UsingConvert();
+            // pr.UsingParse();
+            pr.WorkingWithDate();
+        }
+
+        // Function that not executable
+        public void UsingConvert()
+        {
+            DateTime mydate = Convert.ToDateTime(1000);
+            Console.WriteLine();
+
+            Console.Read();
+        }
+
+        // Converting Date to a String
+        public void UsingParse()
+        {
+            string dateInput = "Jan 1, 2018";
+            DateTime myDate = DateTime.Parse(dateInput);
+            Console.WriteLine(myDate);
+            // DISPLAYS THE DATE IN THE FORMATE 1/1/2018 12:00:00 AM
+        }
+
+        // Getting Today’s date and time
+        public void WorkingWithDate()
+        {
+            Console.WriteLine(DateTime.Today); //Returns the current date
+            Console.WriteLine(DateTime.UtcNow.Date); //Returns the current date
+            Console.WriteLine(DateTime.Now.ToString("HH:mm:ss"));
+            Console.WriteLine(DateTime.Today.ToString("dd-MM-yyyy"));
+            DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+        }
+
+        // Extracting Day, Month and Year from Date
+        public void SplitingDates()
+        {
+            String myDate = DateTime.Now.ToString(); //GET THE CURRENT DATE
+            DateTime datevalue = (Convert.ToDateTime(myDate.ToString())); //CONVERTS THE myDate to a      DateTime TYPE
+
+            String dy = datevalue.Day.ToString();
+            String mn = datevalue.Month.ToString();
+            String yr = datevalue.Year.ToString();
+
+            Console.WriteLine("The day is: " + dy);
+            Console.WriteLine("The month is: " + mn);
+            Console.WriteLine("The year is: " + yr);
+        }
+
+        // About TimeSpan Data Type
+
+        /* According the the msdn documentation, a TimeSpan represents a time interval.
+        Think of TimeSpan as a diffrence in time. When you subtract or add two dates, the result you have will be a TimeSpan. That is why it is called ‘a time interval’.
+
+        Just like a Date contains day, month and year, a TimeSpan contains the following:
+
+            Days
+            Hours
+            Milliseconds
+            Minutes
+            Second */
+
+        public void UsingTimeSpan()
+        {
+            TimeSpan span = DateTime.Now - DateTime.Now.Date;
+            int days = span.Days;
+            int hours = span.Hours;
+            int mins = span.Minutes;
+            int seconds = span.Seconds;
+            int milisecs = span.Milliseconds;
+
+            Console.WriteLine("Days: " + days);
+            Console.WriteLine("Hours: " + hours);
+            Console.WriteLine("Minutes: " + mins);
+            Console.WriteLine("Seconds: " + seconds);
+            Console.WriteLine("Milliseconds: " + milisecs);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# Handling Date and Time Data Types in C# (Gerenciamento de Tipos de Dados de Data e Hora em C#)
+
+Este é um projeto simples em C# que aborda o manuseio de tipos de dados de data e hora, bem como conversões entre eles. Aqui estão alguns dos principais temas abordados neste projeto:
+
+## Conversão de Data para String
+
+Demonstramos como converter um objeto de data para uma representação de string legível.
+
+## Obtendo a Data e Hora Atuais
+
+Mostramos como obter a data e hora atuais usando os recursos oferecidos pela linguagem C#.
+
+## Extração de Dia, Mês e Ano da Data
+
+Exploramos métodos para extrair componentes individuais, como dia, mês e ano, de um objeto de data.
+
+## Sobre o Tipo de Dados TimeSpan
+
+Introduzimos o tipo de dados TimeSpan e como ele pode ser usado para representar intervalos de tempo.
+
+Este projeto serve como um guia introdutório para lidar com dados de data e hora em C#, além de fornecer insights sobre as operações comuns realizadas com esses tipos de dados.
+
+Fique à vontade para explorar o código-fonte e os exemplos fornecidos neste repositório para aprofundar seu entendimento sobre o assunto.
+
+---
+
+**Nota:** Este projeto é apenas para fins educacionais e de demonstração. Se você tiver alguma dúvida ou sugestão, sinta-se à vontade para entrar em contato!
